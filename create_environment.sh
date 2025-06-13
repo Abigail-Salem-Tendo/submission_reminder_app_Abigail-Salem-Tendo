1543ea08457cde1fe52f80463b3d7c8e50232156
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2025-06-13 10:20:25 +0300

    Create the create_environment.sh script that creates a directory and subdirectories

---
+++ create_environment.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#This script creates the environment for the app
+
+#Prompting the user to enter their name
+read -p "Enter your name: " usr_name
+
+#Create the folder with the user's name
+folder="submission_reminder_$usr_name"
+
+#Creating the main directory and subdirectories
+mkdir -p $folder/app
+mkdir -p $folder/modules
+mkdir -p $folder/assets
+mkdir -p $folder/config
