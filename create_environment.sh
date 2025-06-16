d62be4f6c9b9b617da6917683840c52e5d75b1ae
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2025-06-16 16:18:30 +0300

    Populated the modules directory with functions.sh file

--- create_environment.sh
+++ create_environment.sh
@@ -32,5 +32,36 @@ echo "--------------------------------------------"
 check_submissions $submissions_file
 EOF
 
+#Populating the modules directory with the function.sh file
+cat <<EOF > "$main_dir/modules/functions.sh"
+#!/bin/bash
 
+
+# Function to read submissions file and output students who have not submitted
+ function check_submissions {
+   local submissions_file=$1
+   echo "Checking submissions in $submissions_file"
+
+ # Skip the header and iterate through the lines
+   while IFS=, read -r student assignment status; do
+        # Remove leading and trailing whitespace
+        student=$(echo "$student" | xargs)
+        assignment=$(echo "$assignment" | xargs)
+        status=$(echo "$status" | xargs)
+       
+        # Check if assignment matches and status is 'not submitted'
+        if [[ "$assignment" == "$ASSIGNMENT" && "$status" == "not submitted" ]]; then
+              echo "Reminder: $student has not submitted the $ASSIGNMENT assignment!"
+        fi
+   done < <(tail -n +2 "$submissions_file") # Skip the header                                         
+}
+
+
+         
+               
+                                        
+                                           
+
+                                                        
+                                                        
 
