44175d93a6cc215dbda4217ae4cbe6c74fd7134a
Abigail-Salem-Tendo <a.tendo@alustudent.com>
2025-06-16 12:18:46 +0300

    Used EOF to populate the reminder.sh file in the app directory

--- create_environment.sh
+++ create_environment.sh
@@ -5,28 +5,32 @@
 read -p "Enter your name: " usr_name
 
 #Create the folder with the user's name; this will be the parent directory
-folder="submission_reminder_$usr_name"
+main_dir="submission_reminder_$usr_name"
 
 #Creating the parent directory and subdirectories
-mkdir -p $folder/app
-mkdir -p $folder/modules
-mkdir -p $folder/assets
-mkdir -p $folder/config
+mkdir -p "$main_dir/app"
+mkdir -p "$main_dir/modules"
+mkdir -p "$main_dir/assets"
+mkdir -p "$main_dir/config"
 
-#Copying the files that were downloades into the respective subdirectories
-cp /home/abbysalem/reminder.sh $folder/app
-cp /home/abbysalem/functions.sh $folder/modules
-cp /home/abbysalem/submissions.txt $folder/assets
-cp /home/abbysalem/config.env $folder/config
-
-#Creating a script to start the app
-
-cat <<EOF > $folder/startup.sh
+#Now populating the app directory with reminder.sh script
+cat <<EOF > "$main_dir/app/reminder.sh"
 #!/bin/bash
+
+# Source environment variables and helper functions
 source ./config/config.env
 source ./modules/functions.sh
-bash ./app/reminder.sh
+
+# Path to the submissions file
+submissions_file="./assets/submissions.txt"
+
+# Print remaining time and run the reminder function
+echo "Assignment: $ASSIGNMENT"
+echo "Days remaining to submit: $DAYS_REMAINING days"
+echo "--------------------------------------------"
+
+check_submissions $submissions_file
 EOF
 
-#making all .sh file in folder executable
-chmod +x $folder/*.sh
+
+
